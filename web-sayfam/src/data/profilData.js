db6401c7e4f37f9bb6971dee39ce9519b0c1b0ed
alptugozates <omeralptugozates@gmail.com>
2023-12-06 00:16:35 +0300

    profil componenti icin dataları olusturur

---
+++ web-sayfam/src/data/profilData.js
@@ -0,0 +1,19 @@
+export const hakkımda = [
+  {
+    dogumTrh: "25.12.1998",
+    ikametgah: "Adana",
+    egitim: "Çukurova Ünv. İktisat Lisans,2021",
+    rolTercihi: "Full Stack Developer",
+  },
+];
+
+export const aboutMe = [
+  {
+    about:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi molitia.",
+  },
+  {
+    about1:
+      "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+  },
+];
