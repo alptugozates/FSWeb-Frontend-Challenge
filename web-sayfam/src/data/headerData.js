840e1fee0ae0c59751edeabacf12c556e56d5495
alptugozates <omeralptugozates@gmail.com>
2023-12-06 00:16:07 +0300

    header  icin kullanılacak dataları olusturur

---
+++ web-sayfam/src/data/headerData.js
@@ -0,0 +1,17 @@
+import github from "../foto/github.png";
+import linkedin from "../foto/LinkedIn.png";
+import hero from "../foto/hero-right.png";
+
+const headerData = [
+    {
+        baslik: "Alptuğ",
+        aciklama1: "I am a Full Stack Developer...",
+        aciklama2:
+            "...who likes to craft solid and scalable frontend products with great user experiences.",
+        github: github,
+        linkedin: linkedin,
+        heroRigth: hero,
+    },
+];
+
+export default headerData;
