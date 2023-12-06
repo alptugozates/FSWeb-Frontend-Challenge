59e03befc422d1508035b4a26a839981a1337d8d
alptugozates <omeralptugozates@gmail.com>
2023-12-06 20:37:15 +0300

    useState import siler

--- web-sayfam/src/components/Footer.js
+++ web-sayfam/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "../App.css";
 import instagram from "../foto/instagram.svg";
 import codepen from "../foto/codepen.svg";
