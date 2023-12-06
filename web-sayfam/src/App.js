aa7cd3191b0b56640244e84b821b51a22f0e79e8
alptugozates <omeralptugozates@gmail.com>
2023-12-06 14:51:31 +0300

    toastify eklendi

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,11 +1,13 @@
-import "./index.css"
-import './App.css';
+import "./index.css";
+import "./App.css";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
-import Profil from './components/Profil';
-import Project from './components/Project';
-import Footer from './components/Footer';
-import { useEffect, useState } from 'react';
+import Profil from "./components/Profil";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const savedDarkMode = localStorage.getItem("isDarkMode") === "true";
@@ -13,11 +15,38 @@ function App() {
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
-  }
+  };
 
   useEffect(() => {
-    localStorage.setItem("isDarkMode", isDarkMode)
+    localStorage.setItem("isDarkMode", isDarkMode);
   }, [isDarkMode]);
+
+  useEffect(() => {
+    const language = localStorage.getItem("language") || "en";
+    if (language === "tr") {
+      toast.info("Sayfama Hoşgeldiniz!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      toast.info("Welcome to my page!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }, []);
 
   return (
     <div>
@@ -26,6 +55,7 @@ function App() {
       <Profil isDarkMode={isDarkMode} />
       <Project isDarkMode={isDarkMode} />
       <Footer isDarkMode={isDarkMode} />
+      <ToastContainer />
     </div>
   );
 }
