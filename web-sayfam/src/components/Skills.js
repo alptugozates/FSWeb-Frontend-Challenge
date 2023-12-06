28164237b42d9643dca23dbc40730d9fe062267f
alptugozates <omeralptugozates@gmail.com>
2023-12-06 03:05:11 +0300

    responsive düzeltildi

--- web-sayfam/src/components/Skills.js
+++ web-sayfam/src/components/Skills.js
@@ -8,13 +8,15 @@ const Skills = ({ isDarkMode }) => {
   const { t } = useTranslation();
   return (
     <div
-      className={`skills flex ${isDarkMode ? "bg-[#252128]" : "bg-white"
-        }  sm:w-full max-sm:w-full sm:mx-auto  max-sm:mx-auto  lg:flex-row sm:flex-col sm:items-center lg:items-start  sm:py-10 max-sm:py-8 max-sm:flex-col max-sm:items-center`}
+      className={`skills flex ${
+        isDarkMode ? "bg-[#252128]" : "bg-white"
+      }  sm:w-full max-sm:w-full sm:mx-auto  max-sm:mx-auto  lg:flex-row sm:flex-col sm:items-center lg:items-start  sm:py-10 max-sm:py-8 max-sm:flex-col max-sm:items-center`}
     >
       <div className="baslik font-inter font-bold w-1/4 ml-60 lg:text-5xl sm:text-4xl sm:w-1/2 sm:mb-5 text-5xl max-sm:m-0 max-sm:text-2xl max-sm:mb-3">
         <h2
-          className={`ml-32 mt-10 flex flex-grow ${isDarkMode ? "text-custom-green" : "text-custom-blue"
-            }`}
+          className={`ml-32 mt-10 lg:w-9/12 lg:mx-auto lg:mb-8 sm:mx-auto  sm:mb-6  max-sm:m-auto max-sm:mb-5   ${
+            isDarkMode ? "text-custom-green" : "text-custom-blue"
+          }`}
         >
           {t("skills")}
         </h2>
@@ -24,8 +26,9 @@ const Skills = ({ isDarkMode }) => {
           <div className="flex  items-center lg:w-[40%] max-w-[75%] sm:w-[40%]  max-sm:w-[35%]">
             <img className="max-w-[60%] mr-50" src={item.url} alt="" />
             <span
-              className={`max-sm:text-xs font-inter uppercase text-2xl ${isDarkMode ? "text-[#FFF]" : "text-slate-400"
-                }`}
+              className={`max-sm:text-xs font-inter uppercase text-2xl ${
+                isDarkMode ? "text-[#FFF]" : "text-slate-400"
+              }`}
             >
               {item.type}
             </span>
