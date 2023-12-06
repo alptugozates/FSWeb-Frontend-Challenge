1469d8df4b4904c22b74aaf8e37e0a26e2b29df0
alptugozates <omeralptugozates@gmail.com>
2023-12-06 03:04:02 +0300

    aciklama translate ekler

--- web-sayfam/src/data/projectData.js
+++ web-sayfam/src/data/projectData.js
@@ -2,24 +2,23 @@ import project1 from "../foto/project-1.png";
 import project2 from "../foto/project-2.png";
 
 export const p1 = [
-    {
-        react: "react",
-        redux: "redux",
-        vercel: "vercel",
-        image1: project1,
-        aciklama: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
-
-    },
+  {
+    react: "react",
+    redux: "redux",
+    vercel: "vercel",
+    image1: project1,
+    aciklama:
+      "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+  },
 ];
 
 export const p2 = [
-    {
-        react: "react",
-        redux: "redux",
-        vercel: "vercel",
-        image2: project2,
-        aciklama: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
-
-    },
+  {
+    react: "react",
+    redux: "redux",
+    vercel: "vercel",
+    image2: project2,
+    aciklama:
+      "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+  },
 ];
-
